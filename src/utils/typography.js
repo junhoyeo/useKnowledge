511be00b1f46400edf7c1b160dd4b4c878dba9c1
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-21 03:16:53 +0900

    feat: custom typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,11 @@
 import Typography from 'typography';
-import Wordpress2016 from 'typography-theme-wordpress-2016';
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    'a.gatsby-resp-image-link': {
-      boxShadow: 'none',
-    },
-  };
-};
-
-delete Wordpress2016.googleFonts;
-
-const typography = new Typography(Wordpress2016);
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.45,
+  headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Noto Sans KR', 'sans-serif'],
+});
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
