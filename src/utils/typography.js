8a591d4b779186e06ed45f3080352909983dbadf
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2020-01-21 03:30:22 +0900

    feat: add blockquote style

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,6 +5,15 @@ const typography = new Typography({
   baseLineHeight: 1.45,
   headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
   bodyFontFamily: ['Noto Sans KR', 'sans-serif'],
+  overrideThemeStyles: () => ({
+    blockquote: {
+      fontStyle: 'italic',
+      borderLeft: '.25em solid rgba(102, 166, 255, 0.5)',
+      marginLeft: 0,
+      marginRight: 0,
+      padding: '0 1rem',
+    },
+  }),
 });
 
 // Hot reload typography in development.
